1931e302eef171ed2fe3cf1c51a3e550726942c8
Jeongmin Cho <jojm0829@gmail.com>
2023-01-20 01:08:23 +0900

    #7.2 Coin Tracker - Code Challenge

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import { useState, useEffect } from "react";
 function App() {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [price, setPrice] = useState(1);
+  const [amount, setAmount] = useState(1);
+  const onChange = (event) => setAmount(event.target.value);
+  const onSelect = (event) => setPrice(event.target.value);
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
       .then((response) => response.json())
@@ -17,14 +21,32 @@ function App() {
       {loading ? (
         <strong>Loading...</strong>
       ) : (
-        <select>
+        <select onChange={onSelect} value={price}>
+          <option value="xx">Select a coin</option>
           {coins.map((coin) => (
-            <option key={coin.id}>
+            <option value={coin.quotes.USD.price} key={coin.id}>
               {coin.name}({coin.symbol}) : ${coin.quotes.USD.price} USD
             </option>
           ))}
         </select>
       )}
+      <hr />
+      <div>
+        <div>
+          <label htmlFor="USD">USD</label>
+          <input
+            id="USD"
+            onChange={onChange}
+            value={amount}
+            type="number"
+            placeholder="Enter USD"
+          />
+        </div>
+        <div>
+          <label htmlFor="coin">Coin</label>
+          <input id="coin" value={amount / price} type="number" disabled />
+        </div>
+      </div>
     </div>
   );
 }
