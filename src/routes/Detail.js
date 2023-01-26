31e3fa9ddc7873797a93757b578159baee7c1f08
Jeongmin Cho <jojm0829@gmail.com>
2023-01-26 19:56:05 +0900

    #7.9 Detail.module.css 생성 및 수정, download_count를 K+나 M+ 식으로변형하는 함수 작성

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,10 +1,12 @@
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
+import styles from "./Detail.module.css";
 
 function Detail() {
   const { id } = useParams();
   const [loading, setLoading] = useState(true);
   const [movie, setMovie] = useState({});
+  //const [downloadCount, setDownloadCount] = useState("");
   const getMovie = async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
@@ -15,25 +17,42 @@ function Detail() {
   useEffect(() => {
     getMovie();
   }, []);
-  console.log(movie);
+  //console.log(movie);
+
+  function getcount() {
+    if (movie.download_count > 1000000) {
+      return (movie.download_count / 1000000).toFixed(1) + "M+";
+    } else if (movie.download_count > 1000) {
+      return (movie.download_count / 1000).toFixed(1) + "K+";
+    } else {
+      return movie.download_count;
+    }
+  }
+  //console.log(movie.download_count + " : " + downloadCount);
   return (
     <div>
       {loading ? (
         <h1>Loading...</h1>
       ) : (
-        <div>
+        <div className={styles.container}>
           <img src={movie.background_image} alt={movie.title} />
-          <div>
-            <img src={movie.medium_cover_image} alt={movie.title} />
+          <div className={styles.movie}>
+            <img
+              className={styles.img}
+              src={movie.medium_cover_image}
+              alt={movie.title}
+            />
             <div>
               <h1>{movie.title}</h1>
-              <h3>{movie.year}</h3>
-              <h3>
-                {Math.floor(movie.runtime / 60)}h {movie.runtime % 60}min
-              </h3>
-              <h3>Rating: {movie.rating}</h3>
-              <h3>Downloads: {movie.download_count}</h3>
-              <ul>
+              <ul className={styles.info}>
+                <li>{movie.year}</li>
+                <li>
+                  {Math.floor(movie.runtime / 60)}h {movie.runtime % 60}min
+                </li>
+                <li>Rating: {movie.rating}</li>
+                <li>Downloads: {getcount()}</li>
+              </ul>
+              <ul className={styles.genres}>
                 {movie.genres.map((genre, index) => (
                   <li key={index}>{genre}</li>
                 ))}
